62c81af8cdae893447eacdc8f5ab9f99d97a9624
Saksham Sharma <saksham.sharma@procol.in>
2024-11-17 02:02:00 +0530

    Initial commit of Homebrew formula

--- Formula/gpt-cli.rb
+++ Formula/gpt-cli.rb
@@ -2,7 +2,7 @@ class GptCli < Formula
   desc 'A sample GPT CLI tool with pretty responses'
   homepage 'https://github.com/saksham-sharma-99/gpt-cli'
   url 'https://github.com/saksham-sharma-99/gpt-cli/archive/v1.0.0.tar.gz'
-  sha256 '9ecec3744f1ecccea227a5ae510c6d211aabec4f8d8dfbf5a38af1b2'
+  sha256 '8092e99d8448f6b2ddcab94ad47cd04666c7836592ddbed4124a935afd690707'
 
   depends_on 'node'
 
